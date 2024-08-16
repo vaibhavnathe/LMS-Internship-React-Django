ad907b4cb1b33c09b544cc88fa87b1e18e0fe52d
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-16 17:27:54 +0530

    Feat: Add  App.js file which is First component which will render at starting

---
+++ lms_frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Main } from "./components/Main";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Toaster />
+      <Main />
+
+    </div>
+  );
+}
+
+export default App;
