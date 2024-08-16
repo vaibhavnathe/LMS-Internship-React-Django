26f41d194445a3ec111e8a97cbec21e825b56dac
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-16 18:17:30 +0530

    Feat: Add TeacherSidebar Component represents the Teachers Sidebar

---
+++ lms_frontend/src/components/Teacher/TeacherSidebar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function TeacherSidebar() {
+
+    return (
+
+        
+        <div className="card">
+            
+            <div className="list-group list-group-flush">
+
+                <Link to='/teacher-dashboard' className="list-group-item list-group-item-action">Dashboard</Link>
+                <Link to='/teacher-courses' className="list-group-item list-group-item-action">My Courses</Link>
+                <Link to='/add-course' className="list-group-item list-group-item-action">Add Courses</Link>
+                <Link to='/teacher-users' className="list-group-item list-group-item-action">My Users</Link>
+                <Link to='/teacher-profile-setting' className="list-group-item list-group-item-action">Profile Setting</Link>
+                <Link to='/teacher-change-password' className="list-group-item list-group-item-action">Change password</Link>
+                <Link to='/teacher-logout' className="list-group-item list-group-item-action text-danger">Log Out</Link>
+            </div>
+        </div>
+    )
+}
