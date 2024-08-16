5be8922105c864f5abcf54c4660c8d8aa8760478
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-16 17:13:48 +0530

    Feat: Add Serializer file which represents the serializer for each model

---
+++ lms_api/main/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from . import models
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Teacher
+        fields=['id','full_name', 'email', 'password', 'qualification', 'mobile_no','skills']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.CourseCategory
+        fields =['id', 'title', 'description']
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Course
+        fields=['id', 'category', 'teacher', 'title', 'description', 'featured_img', 'techs']
+
+
+class ChapterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Chapter
+        fields=['id', 'course', 'title', 'description', 'video', 'remarks']
