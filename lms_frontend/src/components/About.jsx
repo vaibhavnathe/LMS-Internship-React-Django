892416913ed72331a540a88218a5df79622ce236
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-16 17:37:27 +0530

    Feat : Add About Component

---
+++ lms_frontend/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='container bg-dark text-white mt-5 p-5 rounded'>
+
+        <h2>About Us Page</h2>
+
+    </div>
+  )
+}
