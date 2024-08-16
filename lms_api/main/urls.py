88d86033fba4e3044d86a0a5af281bbe1098ba9f
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-16 17:14:43 +0530

    Feat: Add urls.py file which contains the app level urls and maps urls to views

---
+++ lms_api/main/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Teacher
+    path('teacher/', views.TeacherList.as_view()),
+    path('teacher/<int:pk>/', views.TeacherDetail.as_view()),
+    path('teacher-login', views.teacher_login),
+    
+    # Category
+    path('category/', views.CategoryList.as_view()),
+
+    # Course
+    path('course/', views.CourseList.as_view()),
+
+    # Chapter
+    path('chapter/<int:pk>', views.ChapterDetailView.as_view()),
+
+    # Specific Course Chapter
+    path('course-chapters/<int:course_id>', views.CourseChapterList.as_view()),
+
+    # Teacher Course
+    path('teacher-courses/<int:teacher_id>', views.TeacherCourseList.as_view()),
+]
