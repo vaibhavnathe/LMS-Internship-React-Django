7eedadb43177d1f448b490da69e53019b7aea622
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-16 17:40:56 +0530

    Feat: Add Register component which represents the user sign up page

---
+++ lms_frontend/src/components/User/Register.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default function Register() {
+
+  return (
+    <div className="container mt-4">
+            <div className="row">
+                <div className="col-6 offset-3">
+                    <div className="card">
+                        <h5 className='card-header'>
+                            User Register
+                        </h5>
+                        <div className="card-body">
+                            <form>
+                                <div className="mb-3">
+                                    <label for="exampleInputEmail1" className="form-label">Full Name</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputEmail1" className="form-label">Email</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputEmail1" className="form-label">Username</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputEmail1" className="form-label">Interest</label>
+                                    <textarea className='form-control'></textarea>
+                                    <div className="form-text">Php, Python , Javascript etc.</div>
+                                </div>
+                                
+                                <button type="submit" className="btn btn-primary">Register</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
