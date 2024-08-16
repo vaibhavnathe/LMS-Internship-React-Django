3e22b33adce89cc2b5e2fa35a4502556db745d68
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-16 17:34:42 +0530

    Feat: Add Header component which represents the header of the application

---
+++ lms_frontend/src/components/Header.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+
+    const teacherLoginStatus = localStorage.getItem('teacherLoginStatus');
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+                <div className="container">
+
+                    <Link to='/' className="navbar-brand">
+                        Learn Online
+                    </Link>
+
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ms-auto">
+                            <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
+                            <Link className="nav-link" to='/all-courses'>Courses</Link>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Teacher
+                                </a>
+                                <ul className="dropdown-menu">
+                                    {!teacherLoginStatus && 
+                                        <>
+                                            <li><Link className="dropdown-item" to='/teacher-login'>Login</Link></li>
+                                            <li><Link className="dropdown-item" to='/teacher-register'>Register</Link></li>
+                                        </>
+                                    }
+
+                                    {
+                                        teacherLoginStatus && 
+                                        <>
+                                            <li><Link className="dropdown-item" to='/teacher-dashboard'>Dashboard</Link></li>
+                                            <li><Link className="dropdown-item" to='/teacher-logout'>Log Out</Link></li>
+                                        </>
+                                    }
+                                    
+                                    
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    User
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><Link className="dropdown-item" to='user-login'>Login</Link></li>
+                                    <li><Link className="dropdown-item" to='user-register'>Register</Link></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><Link className="dropdown-item" to='user-dashboard'>Dashboard</Link></li>
+                                    <li><Link className="dropdown-item" to='user-logout'>Log Out</Link></li>
+                                </ul>
+                            </li>
+                            
+                            
+
+                        </div>
+                    </div>
+
+                </div>
+            </nav>
+        </div>
+    )
+}
