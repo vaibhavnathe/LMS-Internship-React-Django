3c2ee45ef0e57af7fcdd869ff6f2f36bcda0df7f
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-19 14:41:49 +0530

    Feat: Made AllCourses Component dynamic by fetching the data from the backend

--- lms_frontend/src/components/AllCourses.jsx
+++ lms_frontend/src/components/AllCourses.jsx
@@ -4,111 +4,68 @@ import logo from '../assests/logo.jpeg'
 import devops from '../assests/devops.png'
 import django from '../assests/django.jpeg'
 import python from '../assests/python.webp'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 
 export default function AllCourses() {
+
+    const [courseData, setCourseData] = useState([]);
+
+    useEffect(() => {
+        const getAllCourse = async () => {
+
+            try {
+                const courses = await axios.get(`${baseUrl}/course`);
+                if (courses) {
+                    setCourseData(courses.data);
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+        getAllCourse();
+
+    }, [])
+
+    console.log(courseData);
 
     return (
         <div className='container mt-4'>
             <h3 className=' pb-1 mb-4'>
                 Latest Courses
             </h3>
-            <div className='row'>
-                <div className='col-md-3  mb-4'>
-                    <div className="card" >
-                        <Link to='/detail/1'>
-                            <img src={devops} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Course title
-                                </h5>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
+            <div className='row mb-4'>
 
-                <div className='col-md-3 mb-4'>
-                    <div className="card" >
-                        <a href="#"><img src={python} className="card-img-top" alt="..." /></a>
-                        <div className="card-body">
-                            <h5 className="card-title">
-                                <a href="#">Course title</a>
-                            </h5>
+
+                {
+                    courseData && courseData.map((course, index) => (
+
+                        <div className='col-md-3  mb-4' key={index}>
+
+                            <div className="card">
+                                <Link to={`/detail/${course.id}`}>
+                                    <img src={course.featured_img} className="card-img-top" alt={course.title} />
+
+
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            {course.title}
+                                        </h5>
+                                    </div>
+                                </Link>
+
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    ))
+                }
 
-                <div className='col-md-3 mb-4'>
-                    <div className="card" >
-                        <a href="#"><img src={django} className="card-img-top" alt="..." /></a>
-                        <div className="card-body">
-                            <h5 className="card-title">
-                                <a href="#">Course title</a>
-                            </h5>
 
-                        </div>
-                    </div>
-                </div>
 
-                <div className='col-md-3 mb-4'>
-                    <div className="card" >
-                        <a href="#"><img src={logo} className="card-img-top" alt="..." /></a>
-                        <div className="card-body">
-                            <h5 className="card-title">
-                                <a href="#">Course title</a>
-                            </h5>
 
-                        </div>
-                    </div>
-                </div>
-                <div className='col-md-3 mb-4'>
-                    <div className="card" >
-                        <Link to='/detail/1'>
-                            <img src={logo} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Course title
-                                </h5>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
-                <div className='col-md-3 mb-4'>
-                    <div className="card" >
-                        <Link to='/detail/1'>
-                            <img src={django} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Course title
-                                </h5>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
-                <div className='col-md-3 mb-4'>
-                    <div className="card" >
-                        <Link to='/detail/1'>
-                            <img src={python} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Course title
-                                </h5>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
-                <div className='col-md-3 mb-4'>
-                    <div className="card" >
-                        <Link to='/detail/1'>
-                            <img src={devops} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    Course title
-                                </h5>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
             </div>
 
             {/* Pagination Start */}
