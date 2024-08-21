9ca342b572bade84c64a90aebe5a2a5454f11ec8
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-21 18:13:53 +0530

    Feat: Made changes in Student Model

--- lms_api/main/models.py
+++ lms_api/main/models.py
@@ -73,10 +73,8 @@ class Student(models.Model):
     full_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    qualification = models.CharField(max_length=200)
-    mobile_no = models.CharField(max_length=20)
-    address = models.TextField()
-    interested = models.TextField()
+    username = models.CharField(max_length=200)
+    interested_categories = models.TextField()
 
     class Meta:
         verbose_name_plural="5. Student"
