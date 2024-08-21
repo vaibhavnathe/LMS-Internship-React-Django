da344dc0834b5eec02d98e398b9895c41a280fb1
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-21 18:21:29 +0530

    Feat: Modified the Register component to handle dynamic registration for students.

--- lms_frontend/src/components/User/Register.jsx
+++ lms_frontend/src/components/User/Register.jsx
@@ -1,45 +1,117 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 export default function Register() {
 
-  return (
-    <div className="container mt-4">
+    const [studentData, setStudentData] = useState({
+        'full_name': '',
+        'email': '',
+        'password': '',
+        'username': '',
+        'interested_categories': '',
+        'status': '',
+    });
+
+    // Change Element value
+    const handleChange = (event) => {
+
+        setStudentData({
+            ...studentData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const submitForm = async (event) => {
+        event.preventDefault(); // Prevent the default form submission
+
+        const studentFormData = new FormData();
+        studentFormData.append("full_name", studentData.full_name);
+        studentFormData.append("email", studentData.email);
+        studentFormData.append("password", studentData.password);
+        studentFormData.append("username", studentData.username);
+        studentFormData.append("interested_categories", studentData.interested_categories);
+
+        try {
+            const response = await axios.post(baseUrl + '/student/', studentFormData, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.status === 201) {
+                console.log(response);
+                setStudentData({
+                    'full_name': '',
+                    'email': '',
+                    'password': '',
+                    'username': '',
+                    'interested_categories': '', 
+                    'status': 'success',
+                });
+                toast.success('Thanks for your Registration!');
+            }
+            else {
+                throw new Error('Unexpected response status');
+            }
+            
+        } catch (error) {
+            console.error(error);
+            setStudentData((prevState) => ({
+                ...prevState,
+                status: 'error'
+            }));
+            toast.error('Something Went Wrong!');
+        }
+    }
+
+    useEffect(() => {
+        document.title = "Student Register"
+    }, [])
+
+
+    return (
+        <div className="container mt-4">
+
             <div className="row">
                 <div className="col-6 offset-3">
                     <div className="card">
                         <h5 className='card-header'>
                             User Register
                         </h5>
-                        <div className="card-body">
-                            <form>
+                        <form onSubmit={submitForm}>
+                            <div className="card-body">
                                 <div className="mb-3">
-                                    <label for="exampleInputEmail1" className="form-label">Full Name</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                    <label htmlFor="full_name" className="form-label">Full Name</label>
+                                    <input required type="text" onChange={handleChange} value={studentData.full_name} className="form-control" name='full_name' />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="exampleInputEmail1" className="form-label">Email</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                    <label htmlFor="email" className="form-label">Email</label>
+                                    <input required type="email" onChange={handleChange} value={studentData.email} className="form-control" name='email' />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="exampleInputEmail1" className="form-label">Username</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                    <label htmlFor="username" className="form-label">Username</label>
+                                    <input required type="text" onChange={handleChange} value={studentData.username} className="form-control" name='username' />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input required type="password" onChange={handleChange} value={studentData.password} className="form-control" name='password' />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="exampleInputEmail1" className="form-label">Interest</label>
-                                    <textarea className='form-control'></textarea>
+                                    <label htmlFor="interested_categories" className="form-label">Interest</label>
+                                    <textarea required className='form-control' onChange={handleChange} value={studentData.interested_categories} name='interested_categories'></textarea>
                                     <div className="form-text">Php, Python , Javascript etc.</div>
                                 </div>
-                                
+
                                 <button type="submit" className="btn btn-primary">Register</button>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
+
                     </div>
                 </div>
             </div>
         </div>
-  )
+    )
 }
