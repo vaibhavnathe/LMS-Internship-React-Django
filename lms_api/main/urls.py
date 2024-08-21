85887e816151615423310e19888b91ba78f05168
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-21 18:16:35 +0530

    Feat: Mapped a path for StudentList View

--- lms_api/main/urls.py
+++ lms_api/main/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('teacher/', views.TeacherList.as_view()),
     path('teacher/<int:pk>/', views.TeacherDetail.as_view()),
     path('teacher-login', views.teacher_login),
+
+
+    # Student
+    path('student/', views.StudentList.as_view()),
     
     # Category
     path('category/', views.CategoryList.as_view()),
