f811ea30dafe2280517ba64bb0932b71088432e7
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-22 12:37:10 +0530

    Feat: Made Login Component dynamic by adding functionality for user login

--- lms_frontend/src/components/Header.jsx
+++ lms_frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
 
     const teacherLoginStatus = localStorage.getItem('teacherLoginStatus');
-
+    const studentLoginStatus = localStorage.getItem('studentLoginStatus');
+    
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -28,7 +29,7 @@ export default function Header() {
                                     Teacher
                                 </a>
                                 <ul className="dropdown-menu">
-                                    {!teacherLoginStatus && 
+                                    {!teacherLoginStatus &&
                                         <>
                                             <li><Link className="dropdown-item" to='/teacher-login'>Login</Link></li>
                                             <li><Link className="dropdown-item" to='/teacher-register'>Register</Link></li>
@@ -36,14 +37,14 @@ export default function Header() {
                                     }
 
                                     {
-                                        teacherLoginStatus && 
+                                        teacherLoginStatus &&
                                         <>
                                             <li><Link className="dropdown-item" to='/teacher-dashboard'>Dashboard</Link></li>
                                             <li><Link className="dropdown-item" to='/teacher-logout'>Log Out</Link></li>
                                         </>
                                     }
-                                    
-                                    
+
+
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
@@ -51,15 +52,27 @@ export default function Header() {
                                     User
                                 </a>
                                 <ul className="dropdown-menu">
-                                    <li><Link className="dropdown-item" to='user-login'>Login</Link></li>
-                                    <li><Link className="dropdown-item" to='user-register'>Register</Link></li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li><Link className="dropdown-item" to='user-dashboard'>Dashboard</Link></li>
-                                    <li><Link className="dropdown-item" to='user-logout'>Log Out</Link></li>
+                                    {
+                                        studentLoginStatus ?
+                                            (
+                                                <>
+                                                    <li><Link className="dropdown-item" to='user-dashboard'>Dashboard</Link></li>
+                                                    <li><Link className="dropdown-item" to='user-logout'>Log Out</Link></li>
+                                                </>
+                                            ) :
+                                            (
+                                                <>
+                                                    <li><Link className="dropdown-item" to='user-login'>Login</Link></li>
+                                                    <li><Link className="dropdown-item" to='user-register'>Register</Link></li>
+                                                </>
+                                            )
+                                    }
+
+
                                 </ul>
                             </li>
-                            
-                            
+
+
 
                         </div>
                     </div>
--- lms_frontend/src/components/User/Login.jsx
+++ lms_frontend/src/components/User/Login.jsx
@@ -1,6 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 export default function Login() {
+
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const navigate = useNavigate();
+
+    const [studentLoginData, setStudentLoginData] = useState({
+        email: '',
+        password: ''
+    });
+
+    // handleChange Handler
+    const handleChange = (event) => {
+        setStudentLoginData({
+            ...studentLoginData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    // Submit Handler
+    const submitForm = async (event) => {
+        event.preventDefault();
+
+        const studentFormData = new FormData();
+        studentFormData.append('email', studentLoginData.email);
+        studentFormData.append('password', studentLoginData.password);
+
+        try {
+            const response = await axios.post(`${baseUrl}/student-login`, studentFormData);
+            console.log(response.data);
+
+            if (response.data.bool == true) {
+                localStorage.setItem('studentLoginStatus', true);
+                localStorage.setItem('studentId', response.data.student_id);
+
+                toast.success('Login Successful!');
+                setTimeout(() => {
+                    window.location.href = '/user-dashboard';
+                }, 1000); // Delay to ensure toast message is visible
+
+            }
+            else {
+                setErrorMsg('Invalid Email or Password!');
+                toast.error('Invalid Email or Password!');
+            }
+
+
+        } catch (error) {
+            console.log(error);
+            toast.error('Login Failed!');
+        }
+    }
+
+    const studentLoginStatus = localStorage.getItem('studentLoginStatus');
+    
+    useEffect(() => {
+        document.title = "Student Login"
+        if (studentLoginStatus === 'true') {
+            window.location.href = '/user-dashboard';
+        }
+    });
+
+
+
     return (
         <div className="container mt-4">
             <div className="row">
@@ -12,18 +80,18 @@ export default function Login() {
                         <div className="card-body">
                             <form>
                                 <div className="mb-3">
-                                    <label for="exampleInputEmail1" className="form-label">Username</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                    <label htmlFor="email" className="form-label">Email</label>
+                                    <input type="email" onChange={handleChange} value={studentLoginData.email} className="form-control" name="email" aria-describedby="emailHelp" />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input type="password" onChange={handleChange} value={studentLoginData.password} className="form-control" name="password" />
                                 </div>
-                                <div className="mb-3 form-check">
-                                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                                        <label className="form-check-label" for="exampleCheck1">Remember Me</label>
-                                </div>
-                                <button type="submit" className="btn btn-primary">Login</button>
+                                {/* <div className="mb-3 form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                    <label className="form-check-label" htmlFor="exampleCheck1">Remember Me</label>
+                                </div> */}
+                                <button type="submit" onClick={submitForm} className="btn btn-primary">Login</button>
                             </form>
                         </div>
                     </div>
