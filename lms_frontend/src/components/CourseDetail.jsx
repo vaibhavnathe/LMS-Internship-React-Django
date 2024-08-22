27da2c097cddc2715559ab2f5142cc07d545177f
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-22 18:21:51 +0530

    Feat: Added the Functionality of  Course enrollement for specific student with validations

--- lms_frontend/src/components/CourseDetail.jsx
+++ lms_frontend/src/components/CourseDetail.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 import logo from '../assests/logo.jpeg'
 import axios from 'axios';
 import ReactPlayer from 'react-player';
+import toast from 'react-hot-toast';
 
 
 const baseUrl = 'http://127.0.0.1:8000/api';
@@ -11,11 +12,15 @@ const siteUrl = 'http://127.0.0.1:8000/';
 export default function CourseDetail() {
 
     const { course_id } = useParams();
+    const studentId = localStorage.getItem('studentId');
+    const navigate = useNavigate();
 
     const [courseData, setCourseData] = useState(null);
     const [chapterData, setChapterData] = useState([]);
     const [relatedCourseData, setRelatedCourseData] = useState([]);
     const [techList, setTechList] = useState([]);
+    const [userLoginStatus, setuUserLoginStatus] = useState();
+    const [enrollStatus, setEnrollStatus] = useState();
 
     useEffect(() => {
         const fetchCourseData = async () => {
@@ -35,6 +40,41 @@ export default function CourseDetail() {
             }
         }
         fetchCourseData();
+
+        const fetchEnrollStatus = async () => {
+            if (!studentId) {
+                // toast("Login First to enrolled in this Course!", {
+                //     icon: '⚠️',
+                //     style: {
+                //         borderRadius: '10px',
+                //         background: '#20c997    ',
+                //         color: '#fff',
+                //     },
+                // });
+                
+                return;
+            }
+
+            try {
+                const response = await axios.get(`${baseUrl}/fetch-enroll-status/${studentId}/${course_id}`);
+                if (response.data.bool == true) {
+                    // console.log(response.data)
+                    setEnrollStatus('success');
+                }
+            }
+            catch (error) {
+                console.log("Error While Fetching Course Enroll Status!");
+                console.log(error);
+            }
+        }
+
+        fetchEnrollStatus();
+
+        const studentLoginStatus = localStorage.getItem('studentLoginStatus');
+        if (studentLoginStatus === 'true') {
+            setuUserLoginStatus('success');
+        }
+
     }, [])
 
     // console.log(courseData);
@@ -46,6 +86,47 @@ export default function CourseDetail() {
         return <div>Loading...</div>;
     }
 
+    //Course Enroll Handler
+    const enrollCourse = async () => {
+
+        if (!studentId) {
+            toast("Login First!", {
+                icon: '⚠️',
+                style: {
+                    borderRadius: '10px',
+                    background: '#20c997    ',
+                    color: '#fff',
+                },
+            });
+            setTimeout(() => {
+                navigate("/user-login");
+            }, 1000);
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('course', course_id);
+        formData.append('student', studentId);
+
+        try {
+            const response = await axios.post(baseUrl + '/student-enroll-course/', formData, {
+                headers: {
+                    'content-type': 'multipart/form-data'
+                }
+            });
+
+            if (response) {
+                setEnrollStatus('success')
+                toast.success("Successfully Enrolled!");
+                console.log(response.data);
+            }
+
+        }
+        catch (error) {
+            toast.error("Something Went Wrong!");
+            console.log(error);
+        }
+    }
 
     return (
         <div className="container mt-3">
@@ -65,80 +146,92 @@ export default function CourseDetail() {
                             {
                                 techList.map((tech, index) => (
                                     <Link to={`/category/${tech.trim()}`} key={index} className='badge bg-warning ms-1 badge-pill text-dark'>
-                                        {tech} 
+                                        {tech}
                                     </Link>
                                 ))
                             }
-                            
-                         </p>
+
+                        </p>
 
                     }
                     <p className='fw-bold'>Duration: 3 Hours 30 Minutes</p>
                     <p className='fw-bold'>Total Enrolled: 456 Students</p>
                     <p className='fw-bold'>Rating: 4.5/5</p>
+                    {
+                        userLoginStatus === 'success'
+                            ? enrollStatus === 'success' ? (<p><span>You are already enrolled in this course!</span></p>)
+                                : (<p><button type='button' onClick={enrollCourse} className='btn btn-success btn-md'>Enroll in this Course</button></p>)
+                            : (<p><Link to='/user-login'>Please Login to Enroll in this Course!</Link></p>)
+                    }
+
+
                 </div>
             </div>
 
             {/* Course Videos*/}
-            <div className="card mt-4">
-                <h5 className="card-header">
-                    In this course
-                </h5>
-                <ul className="list-group list-group-flush">
-                    {
-                        (chapterData.length > 0) && (
-                            chapterData.map((chapter, index) => (
+            {
+                userLoginStatus === 'success' && enrollStatus === 'success' &&
+                <div className="card mt-4">
+                    <h5 className="card-header">
+                        In this course
+                    </h5>
+                    <ul className="list-group list-group-flush">
+                        {
+                            (chapterData.length > 0) && (
+                                chapterData.map((chapter, index) => (
 
-                                <li className="list-group-item" key={index}>
-                                    {chapter.title}
+                                    <li className="list-group-item" key={index}>
+                                        {chapter.title}
 
-                                    <span className='float-end'>
-                                        <span className='me-5'>1 Hour 30 Minutes</span>
-                                        <button className="btn btn-sm btn-danger float-end" data-bs-toggle="modal" data-bs-target="#videoModal1"><i className="bi bi-youtube"></i></button>
-                                    </span>
+                                        <span className='float-end'>
+                                            <span className='me-5'>1 Hour 30 Minutes</span>
+                                            <button className="btn btn-sm btn-danger float-end" data-bs-toggle="modal" data-bs-target="#videoModal1"><i className="bi bi-youtube"></i></button>
+                                        </span>
 
-                                    {/* <!-- Video Modal  --> */}
-                                    <div className="modal fade" id="videoModal1" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div className="modal-dialog modal-xl">
-                                            <div className="modal-content">
-                                                <div className="modal-header">
-                                                    <h1 className="modal-title fs-5" id="exampleModalLabel">Video 1</h1>
-                                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-                                                <div className="modal-body">
-                                                    <div className="ratio ratio-16x9">
-                                                        {chapter.video ? (
-                                                            // <iframe
-                                                            //     src={chapter.video}
-                                                            //     title={chapter.title}
-                                                            //     allowFullScreen
-                                                            // ></iframe>
-                                                            <div>
-                                                                <ReactPlayer url={chapter.video} controls={true} width="100%" height="100%" />
-                                                                {/* <video controls width="100%" height="auto"/>
-                                                                <source src={chapter.video} type='video/mp4' /> */}
-                                                            </div>
-                                                        ) : (
-                                                            <p>No video available</p>
-                                                            // 
-                                                        )}
-
+                                        {/* <!-- Video Modal  --> */}
+                                        <div className="modal fade" id="videoModal1" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div className="modal-dialog modal-xl">
+                                                <div className="modal-content">
+                                                    <div className="modal-header">
+                                                        <h1 className="modal-title fs-5" id="exampleModalLabel">Video 1</h1>
+                                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
-                                                </div>
+                                                    <div className="modal-body">
+                                                        <div className="ratio ratio-16x9">
+                                                            {chapter.video ? (
+                                                                // <iframe
+                                                                //     src={chapter.video}
+                                                                //     title={chapter.title}
+                                                                //     allowFullScreen
+                                                                // ></iframe>
+                                                                <div>
+                                                                    <ReactPlayer url={chapter.video} controls={true} width="100%" height="100%" />
+                                                                    {/* <video controls width="100%" height="auto"/>
+                                                                <source src={chapter.video} type='video/mp4' /> */}
+                                                                </div>
+                                                            ) : (
+                                                                <p>No video available</p>
+                                                                // 
+                                                            )}
 
+                                                        </div>
+                                                    </div>
+
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
 
 
-                                </li>
-                            ))
-                        )
-                    }
+                                    </li>
+                                ))
+                            )
+                        }
 
 
-                </ul>
-            </div>
+                    </ul>
+                </div>
+            }
+
 
             <h3 className=' pb-1 mb-4 mt-4'>
                 Related Courses
