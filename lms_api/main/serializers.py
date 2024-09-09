afb40e491a39ab5c5115c9e1613e00500a8dab25
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-09-09 11:43:51 +0530

    Feat: Added the total_enrolled_students field into CourseSerializer

--- lms_api/main/serializers.py
+++ lms_api/main/serializers.py
@@ -22,7 +22,7 @@ class CategorySerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model=models.Course
-        fields=['id', 'category', 'teacher', 'title', 'description', 'featured_img', 'techs', 'course_chapters', 'related_videos', 'tech_list']
+        fields=['id', 'category', 'teacher', 'title', 'description', 'featured_img', 'techs', 'course_chapters', 'related_videos', 'tech_list', 'total_enrolled_students']
         depth=1
 
 
@@ -35,3 +35,4 @@ class StudentCourseEnrollSerializer(serializers.ModelSerializer):
     class Meta:
         model=models.StudentCourseEnrollment
         fields =['id', 'course', 'student', 'enrolled_time']
+        depth=1
