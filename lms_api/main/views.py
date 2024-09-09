7a2bc8c75d766ac2289c97e6be11655c15e5b5b3
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-09-09 11:44:53 +0530

    Feat: created the EnrolledStudentList View to fetch the total enrolled students of the specific course

--- lms_api/main/views.py
+++ lms_api/main/views.py
@@ -156,3 +156,12 @@ def fetch_enroll_status(request, student_id, course_id):
         return JsonResponse({'bool':True})
     else:
         return JsonResponse({'bool':False})
+
+class EnrolledStudentList (generics.ListAPIView):
+    queryset = models.StudentCourseEnrollment.objects.all()
+    serializer_class = StudentCourseEnrollSerializer
+
+    def get_queryset(self):
+        course_id = self.kwargs['course_id']
+        course = models.Course.objects.get(pk=course_id)
+        return models.StudentCourseEnrollment.objects.filter(course = course)
