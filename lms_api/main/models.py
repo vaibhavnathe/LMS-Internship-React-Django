b43dd343ab17eabcfaef20b1eb8e369ccae7879a
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-09-26 14:27:42 +0530

    Feat: Modified Teacher Model

--- lms_api/main/models.py
+++ lms_api/main/models.py
@@ -5,11 +5,11 @@ from django.core import serializers
 # Teacher Model
 class Teacher(models.Model):
     full_name = models.CharField(max_length=100)
-    detail = models.TextField(null=True)
     email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=100, blank=True, null=True)
     qualification = models.CharField(max_length=200)
     mobile_no = models.CharField(max_length=20)
+    profile_img = models.ImageField(upload_to='teacher_profile_imgs/' , null=True)
     skills = models.TextField()
 
     class Meta:
