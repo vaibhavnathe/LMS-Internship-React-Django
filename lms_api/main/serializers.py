31fbce7bae896c577a8e40f9c90a5a892517ab19
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-09-27 14:57:20 +0530

    Feat: Add StudentFavouriteCourseSerializer in serializers.py file

--- lms_api/main/serializers.py
+++ lms_api/main/serializers.py
@@ -64,6 +64,18 @@ class StudentCourseEnrollSerializer(serializers.ModelSerializer):
             if request and request.method == 'GET':
                 self.Meta.depth = 2
 
+class StudentFavouriteCourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.StudentFavouriteCourse
+        fields =['id', 'course', 'student', 'status']
+
+    def __init__(self, *args, **kwargs):
+            super(StudentFavouriteCourseSerializer, self).__init__(*args, **kwargs)
+            request = self.context.get('request')
+            self.Meta.depth = 0
+            if request and request.method == 'GET':
+                self.Meta.depth = 2
+
 class CourseRatingSerializer(serializers.ModelSerializer):
     class Meta:
         model=models.CourseRating
