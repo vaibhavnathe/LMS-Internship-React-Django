86ea19faab351df528e8a2131d79ce02f69a8c6b
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-02 15:59:54 +0530

    Feat: Made the Student Dashboard dynamic by adding the functionality

--- lms_frontend/src/components/User/Dashboard.jsx
+++ lms_frontend/src/components/User/Dashboard.jsx
@@ -1,16 +1,76 @@
-import React from 'react' 
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import Sidebar from './Sidebar'
+import { Link, useParams } from 'react-router-dom'
+import axios from 'axios';
+
+import toast, { Toaster } from 'react-hot-toast';
+
+const baseUrl = 'http://127.0.0.1:8000/api';
 
 export default function Dashboard() {
+
+    const [dashboardData, setDashboardData] = useState([]);
+    const studentId = localStorage.getItem('studentId');
+
+    useEffect(() => {
+        // Fetch Courses
+        const fetchCourses = async() => {
+            try{
+                const res = await axios.get(`${baseUrl}/student/dashboard/${studentId}`)
+                if(res){
+                    // console.log("result : ", res);
+                    setDashboardData(res.data);
+                }
+            }
+            catch(error){
+                console.log(error);
+            }
+        }
+
+        fetchCourses();
+    },[])
+    
     return (
         <div className='container mt-4'>
+            
             <div className="row">
                 <aside className='col-md-3'>
                     <Sidebar/>
                 </aside>
                 <section className='col-md-9'>
-                  Dashboard
+                    <div className='row'>
+                        <div className='col-md-4'>
+                            <div className='card border-primary'>
+                                <h5 className='card-header bg-primary text-white'>Enrolled Courses</h5>
+                                <div className='card-body'>
+                                    <h3><Link to='/my-courses'>{dashboardData.enrolled_courses}</Link></h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='col-md-4'>
+                            <div className='card border-success'>
+                                <h5 className='card-header bg-success text-white'>Favourite Courses</h5>
+                                <div className='card-body'>
+                                    <h3><Link to='/favourite-courses'>{dashboardData.favourite_courses}</Link></h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='col-md-4'>
+                            <div className='card border-info'>
+                                <h5 className='card-header bg-info text-white'>Assignments</h5>
+                                <div className='card-body'>
+                                    <h5>
+                                        <Link to='/my-assignments'>
+                                            Completed : {dashboardData.complete_assignments}, 
+                                            Pending : {dashboardData.pending_assignments}
+                                        </Link>
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
