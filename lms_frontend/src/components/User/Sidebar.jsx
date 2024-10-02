bbb5eb588afef7d4470f8108ba911839fa75eb2f
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-02 14:51:51 +0530

    Feat: Modified Sidebar Component of TeacherDashboard with small changes

--- lms_frontend/src/components/User/Sidebar.jsx
+++ lms_frontend/src/components/User/Sidebar.jsx
@@ -10,11 +10,11 @@ export default function Sidebar() {
             
             <div className="list-group list-group-flush">
 
-                <Link to='/user-dashboard' className="list-group-item list-group-item-action">Dashboard</Link>
+                <Link to='/user-dashboard' className="list-group-item list-group-item-action text-b"><strong>Dashboard</strong></Link>
                 <Link to='/my-courses' className="list-group-item list-group-item-action">My Courses</Link>
                 <Link to='/favourite-courses' className="list-group-item list-group-item-action">Favourite Courses</Link>
                 <Link to='/recommended-courses' className="list-group-item list-group-item-action">Recommended Courses</Link>
-                <Link to='/my-assignments' className="list-group-item list-group-item-action">Teacher Assignments</Link>
+                <Link to='/my-assignments' className="list-group-item list-group-item-action">Assignments</Link>
                 <Link to='/profile-setting' className="list-group-item list-group-item-action">Profile Setting</Link>
                 <Link to='/change-password' className="list-group-item list-group-item-action">Change password</Link>
                 <Link to='/user-logout' className="list-group-item list-group-item-action text-danger">Log Out</Link>
