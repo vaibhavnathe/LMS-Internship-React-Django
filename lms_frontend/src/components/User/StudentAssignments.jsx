117ad95abee55a1db5b4a5f4d715c760de9891e1
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-02 14:53:04 +0530

    Feat: Added the Assignments Completion functinality in StudentAssignments Component

--- lms_frontend/src/components/User/StudentAssignments.jsx
+++ lms_frontend/src/components/User/StudentAssignments.jsx
@@ -2,6 +2,7 @@ import Sidebar from './Sidebar'
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const baseUrl = 'http://127.0.0.1:8000/api';
 
@@ -10,6 +11,7 @@ export default function StudentAssignments() {
 
     const student_id = localStorage.getItem('studentId');
     const [assignmentData, setAssignmentData] = useState([]);
+    const [assignmentStatus, setAssignmentStatus] = useState('');
 
     // Fetch all the courses
     useEffect(() => {
@@ -30,6 +32,40 @@ export default function StudentAssignments() {
 
     }, [])
 
+
+    const markAsDone = async (assignment_id, title, detail, student, teacher) => {
+
+        const formData = new FormData();
+        formData.append('student_status', true);
+        formData.append('title', title);
+        formData.append('detail', detail);
+        formData.append('student', student);
+        formData.append('teacher', teacher);
+
+        try {
+            const response = await axios.put(baseUrl + '/update-assignment/' + assignment_id, formData, {
+                headers: {
+                    'content-type': 'multipart/form-data'
+                }
+            });
+
+            if (response.status == 200 || response.status == 201) {
+                setAssignmentStatus('success');
+                toast.success("You have Successfully Completed this Assignment");
+                console.log(response.data);
+     
+                setTimeout(() => {
+                    window.location.reload();
+                }, 2000); // Adjust the delay time (in milliseconds) as needed
+            }
+
+        }
+        catch (error) {
+            toast.error("Something Went Wrong!");
+            console.log(error);
+        }
+    }
+
     return (
 
         <div className='container mt-4'>
@@ -47,6 +83,7 @@ export default function StudentAssignments() {
                                         <th>Title</th>
                                         <th>Detail</th>
                                         <th>Teacher</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -57,6 +94,15 @@ export default function StudentAssignments() {
                                                     <td>{assignment.title}</td>
                                                     <td>{assignment.detail}</td>
                                                     <td><Link to={`/teacher-detail/${assignment.teacher.id}`}> {assignment.teacher.full_name}</Link></td>
+                                                    <td>
+                                                        {
+                                                            assignment.student_status == true
+                                                                
+                                                                ? ((<span className='badge bg-primary'>Completed</span>))
+                                                                :(<button onClick={() => markAsDone(assignment.id, assignment.title, assignment.detail, assignment.student.id, assignment.teacher.id)}
+                                                                className='btn btn-success btn-sm'>Mark as Done</button>)
+                                                        }
+                                                    </td>
                                                 </tr>
                                             ))
                                         )
