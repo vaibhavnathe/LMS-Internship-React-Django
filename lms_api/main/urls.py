089f28be8c7d4a670a74cb8bd4a1efe24a75bd04
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-05 18:26:25 +0530

    Feat: Mapped the path for QuizList view inside the urls.py file

--- lms_api/main/urls.py
+++ lms_api/main/urls.py
@@ -37,27 +37,19 @@ urlpatterns = [
 
     # Course
     path('course/', views.CourseList.as_view()),
-
-     # Specific Course detail
     path('course/<int:pk>', views.CourseDetailView.as_view()),
-
-    # Chapter
     path('chapter/<int:pk>', views.ChapterDetailView.as_view()),
-
-    # Specific Course Chapter
     path('course-chapters/<int:course_id>', views.CourseChapterList.as_view()),
-
-    # Teacher Course
     path('teacher-courses/<int:teacher_id>', views.TeacherCourseList.as_view()),
-
-    # Course detail
     path('teacher-course-detail/<int:pk>', views.TeacherCourseDetail.as_view()),
-
-    # Course Rating
      path('course-rating/', views.CourseRatingList.as_view()),
 
     #  Notification
     path('student/fetch-all-notifications/<int:student_id>', views.NotificationList.as_view()),
     path('save-notification/', views.NotificationList.as_view()),
+
+    # Quiz
+    path('quiz/', views.QuizList.as_view()),
+
 
 ]
