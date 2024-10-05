21668d21ea5afc430327b76a14f426b044a8e2fd
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-05 17:31:26 +0530

    Feat: Created the Quiz, QuizQuetions and CourseQuiz model for quizes

--- lms_api/main/models.py
+++ lms_api/main/models.py
@@ -199,3 +199,36 @@ class Notification(models.Model):
 
     class Meta:
         verbose_name_plural="10. Notifications"
+
+
+# Quiz Model
+class Quiz(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=200)
+    detail = models.TextField()
+    add_time = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural="11. Quiz"
+
+# Quiz Quetions Model
+class QuizQuetions(models.Model):
+    Quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True)
+    quetions = models.CharField(max_length=200)
+    ans1 = models.CharField(max_length=200)
+    ans2 = models.CharField(max_length=200)
+    ans3 = models.CharField(max_length=200)
+    ans4 = models.CharField(max_length=200)
+    right_ans = models.CharField(max_length=200)
+    add_time = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural="12. Quiz Quetions"
+
+# Add Quiz to Course
+class CourseQuiz(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    Quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural="13. Course Quiz"
