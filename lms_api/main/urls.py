50eaf8f28380f603395fd42bb1034df38b87a1bc
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-09 12:46:28 +0530

    Feat: Mapped QuizDetailView & TeacherQuizDetail views with specific path in urls.py file

--- lms_api/main/urls.py
+++ lms_api/main/urls.py
@@ -51,6 +51,8 @@ urlpatterns = [
     # Quiz
     path('quiz/', views.QuizList.as_view()),
     path('teacher-quiz/<int:teacher_id>', views.TeacherQuizList.as_view()),
+    path('teacher-quiz-detail/<int:pk>', views.TeacherQuizDetail.as_view()),
+    path('quiz/<int:pk>', views.QuizDetailView.as_view()),
 
 
 ]
