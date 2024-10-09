0476c122dccdf81c3c6fc1dcd60fe73aa10141d8
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-09 14:58:49 +0530

    Feat: Add QuestionSerializer for QuizQuestions model in serializers.py file

--- lms_api/main/serializers.py
+++ lms_api/main/serializers.py
@@ -122,4 +122,15 @@ class QuizSerializer(serializers.ModelSerializer):
             self.Meta.depth = 0
             if request and request.method == 'GET':
                 self.Meta.depth = 2
-    
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.QuizQuestions
+        fields=['id', 'quiz', 'question', 'ans1', 'ans2', 'ans3','ans4', 'right_ans']
+    def __init__(self, *args, **kwargs):
+            super(QuestionSerializer, self).__init__(*args, **kwargs)
+            request = self.context.get('request')
+            self.Meta.depth = 0
+            if request and request.method == 'GET':
+                self.Meta.depth = 1
