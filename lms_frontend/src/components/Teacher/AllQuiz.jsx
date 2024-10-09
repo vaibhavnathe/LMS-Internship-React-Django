fadb2cef44ff5f8e900eeae2bc949b7eaa37d951
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-09 12:43:36 +0530

    Feat: Added the functionality of deleting quiz of specific Teacher in AllQuiz Component

--- lms_frontend/src/components/Teacher/AllQuiz.jsx
+++ lms_frontend/src/components/Teacher/AllQuiz.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import TeacherSidebar from './TeacherSidebar'
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const baseUrl = 'http://127.0.0.1:8000/api';
 
@@ -10,6 +11,7 @@ export default function AllQuiz() {
     const teacherId = localStorage.getItem('teacherId');
 
     const [quizData, setQuizData] = useState([]);
+    const [totalResult, setTotalResult] = useState(0);
 
     useEffect(() => {
         const getAllCourse = async () => {
@@ -17,7 +19,7 @@ export default function AllQuiz() {
             try {
                 const quizes = await axios.get(baseUrl + `/teacher-quiz/${teacherId}`);
                 if (quizes) {
-                    console.log("Data : ", quizes);
+                    // console.log("Data : ", quizes);
                     setQuizData(quizes.data);
                 }
             }
@@ -28,6 +30,42 @@ export default function AllQuiz() {
         getAllCourse();
 
     }, [])
+
+    // Delete chapter data
+    const handleDeleteClick = async (quiz_id) => {
+        const result = await Swal.fire({
+            title: 'Confirm',
+            text: 'Are you sure you want to delete this data',
+            icon: 'info',
+            confirmButtonText: 'Continue',
+            showCancelButton: true
+        })
+
+        if (result.isConfirmed) {
+            try {
+                const res = await axios.delete(`${baseUrl}/quiz/${quiz_id}`);
+
+                if (res) {
+                    Swal.fire("Success", "Data has been deleted!")
+                    try {
+                        const quizes = await axios.get(baseUrl + `/teacher-quiz/${teacherId}`);
+                        if (quizes) {
+                            setQuizData(quizes.data);
+                            setTotalResult(quizes.data.length);
+                        }
+                    }
+                    catch (error) {
+                        console.log(error);
+                    }
+                }
+
+            }
+            catch (error) {
+                Swal.fire("error", "Data has not been deleted!")
+            }
+
+        }
+    }
 
 
     return (
@@ -64,9 +102,9 @@ export default function AllQuiz() {
                                                     <td><Link to={`#`}> 123 </Link></td>
                                                     <td>
                                                        
-                                                        <Link to='#'><button className='btn btn-info btn-md  '>Edit</button></Link>
+                                                        <Link to={`/edit-quiz/${quiz.id}`}><button className='btn btn-info btn-md  '>Edit</button></Link>
                                                         <Link to={`/add-quiz-quetion/${quiz.id}`}><button className='btn btn-success btn-md  ms-2 '>Add Quetion</button></Link>
-                                                        <button className='btn btn-danger btn-md ms-2'>Delete</button>
+                                                        <button onClick={() => handleDeleteClick(quiz.id)} className='btn btn-danger btn-md ms-2'>Delete</button>
 
                                                     </td>
                                                 </tr>
