817c56444e6ede235552d39dd2dc34535013223c
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-10-10 18:13:09 +0530

    Feat: Mapped path for QuizQuestionsList, AttemptQuizList and QuizQuestionsList view in url.py file

--- lms_api/main/urls.py
+++ lms_api/main/urls.py
@@ -55,9 +55,12 @@ urlpatterns = [
     path('teacher-quiz-detail/<int:pk>', views.TeacherQuizDetail.as_view()),
     path('quiz/<int:pk>', views.QuizDetailView.as_view()),
     path('quiz-questions/<int:quiz_id>', views.QuizQuestionsList.as_view()),
+    path('quiz-questions/<int:quiz_id>/<int:limit>', views.QuizQuestionsList.as_view()),
     path('quiz-assign-course/', views.CourseQuizList.as_view(),name='student-enroll-course'),
     path('fetch-quiz-assign-status/<int:quiz_id>/<int:course_id>', views.fetch_quiz_assign_status,name='student-enroll-course'),
     path('fetch-assigned-quiz/<int:course_id>', views.CourseQuizList.as_view()),
+    path('attempt-quiz/', views.AttemptQuizList.as_view()),
+    path('quiz-questions/<int:quiz_id>/next-question/<int:question_id>', views.QuizQuestionsList.as_view()),
 
 
 ]
